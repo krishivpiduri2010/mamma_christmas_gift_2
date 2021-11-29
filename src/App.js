8dff66eef996fb24a4dcba146d1b2e4cfd44b2ba
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2021-11-29 15:49:52 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen backdrop-filter backdrop-grayscale'>
+      <div className='text-center text-5xl text-bold'>This is my website</div>
+      <div className='text-center py-3'>This is the website I asked you to look at</div>
     </div>
   );
 }
